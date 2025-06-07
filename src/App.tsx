5bcc200febb0532f348553add2cf4205d232d8e9
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-06-07 13:35:06 -0400

    Update app.tsx

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-// import { GameConsole } from './components/game-console';
-import { GameTitle } from './components/game-title';
-
-export default function App() {
-    // return <GameConsole />;
-    return <GameTitle />;
-}
---
+++ src/app.tsx
@@ -0,0 +1,5 @@
+import { GameConsole } from './components/game-console';
+
+export default function App() {
+    return <GameConsole />;
+}
