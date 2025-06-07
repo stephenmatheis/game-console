aacc0e22f7797d8c6da19138557b15857abfd2fe
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-06-07 01:55:39 -0400

    feat: arrows

--- src/components/game-title/game-title.tsx
+++ src/components/game-title/game-title.tsx
@@ -30,6 +30,7 @@ import { useState } from 'react';
 
 export function GameTitle() {
     const [start, setStart] = useState(false);
+    const [selected, setSelected] = useState<'New Game' | 'Load'>('New Game');
 
     return (
         <>
@@ -90,18 +91,45 @@ export function GameTitle() {
                         </div>
                     </motion.div>
                     <div className={styles.buttons}>
+                        <motion.div
+                            className={styles.left}
+                            initial={{ opacity: 0, scale: 0.5 }}
+                            animate={{ opacity: 1, scale: 1 }}
+                            transition={{ type: 'spring', stiffness: 300, damping: 15, delay: 2.2 }}
+                            style={{ top: selected === 'New Game' ? 0 : 32 }}
+                        >
+                            ◄
+                        </motion.div>
+                        <motion.div
+                            className={styles.right}
+                            initial={{ opacity: 0, scale: 0.5 }}
+                            animate={{ opacity: 1, scale: 1 }}
+                            transition={{ type: 'spring', stiffness: 300, damping: 15, delay: 2.2 }}
+                            style={{ top: selected === 'New Game' ? 0 : 32 }}
+                        >
+                            ►
+                        </motion.div>
                         <motion.button
                             className={styles.start}
                             onClick={() => {
                                 setStart(true);
                             }}
-                            whileHover={{
-                                scale: 1.2,
-                                transition: { type: 'spring', stiffness: 300, damping: 20 },
-                            }}
                             initial={{ opacity: 0, scale: 0.5 }}
-                            animate={{ opacity: 1, scale: 1 }}
-                            transition={{ type: 'spring', stiffness: 300, damping: 15, delay: 3 }}
+                            animate={{
+                                opacity: 1,
+                                scale: 1,
+                                transition: {
+                                    opacity: { type: 'spring', stiffness: 300, damping: 15, delay: 1.5 },
+                                    scale: { type: 'spring', stiffness: 300, damping: 15, delay: 1.5 },
+                                },
+                            }}
+                            whileHover={{
+                                color: 'rgb(255, 204, 0)',
+                                transition: { ease: 'easeInOut', duration: 0.2 },
+                            }}
+                            onHoverStart={() => {
+                                setSelected('New Game');
+                            }}
                         >
                             New Game
                         </motion.button>
@@ -110,13 +138,22 @@ export function GameTitle() {
                             onClick={() => {
                                 setStart(false);
                             }}
-                            whileHover={{
-                                scale: 1.2,
-                                transition: { type: 'spring', stiffness: 300, damping: 20 },
-                            }}
                             initial={{ opacity: 0, scale: 0.5 }}
-                            animate={{ opacity: 1, scale: 1 }}
-                            transition={{ type: 'spring', stiffness: 300, damping: 15, delay: 3.2 }}
+                            animate={{
+                                opacity: 1,
+                                scale: 1,
+                                transition: {
+                                    opacity: { type: 'spring', stiffness: 300, damping: 15, delay: 1.5 },
+                                    scale: { type: 'spring', stiffness: 300, damping: 15, delay: 1.5 },
+                                },
+                            }}
+                            whileHover={{
+                                color: 'rgb(255, 204, 0)',
+                                transition: { ease: 'easeInOut', duration: 0.2 },
+                            }}
+                            onHoverStart={() => {
+                                setSelected('Load');
+                            }}
                         >
                             Load
                         </motion.button>
